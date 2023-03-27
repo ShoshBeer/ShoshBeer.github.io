221c7a566e4ea7521c4eb6a85ce7d1092cb1f79a
ShoshBeer <shoshanabeer@gmail.com>
2023-03-27 18:57:54 -0400

    set up tailwind

--- shosh-portfolio/pages/index.js
+++ shosh-portfolio/pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -9,10 +10,11 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>I control this body I'm typing with. You can call either one of us Shoshana, or Shoeshine if you prefer.</p>
+        <p className="text-xl font-bold">I control this body I'm typing with. You can call either one of us Shoshana, or Shoeshine if you prefer.</p>
         <p>
          I usually use money to acquire goods and services, but eventually I'll use goods and services.
         </p>
+        <Link href="/projects/talking-in-circles">Go to Project 1</Link>
       </section>
     </Layout>
   );
