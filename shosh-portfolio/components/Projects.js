9bdcf69d1131b629daa7f75df34d59be747e8ab8
ShoshBeer <shoshanabeer@gmail.com>
2023-04-07 19:57:10 -0400

    make project section more responsive

--- shosh-portfolio/components/Projects.js
+++ shosh-portfolio/components/Projects.js
@@ -1,5 +1,9 @@
 import react from "react";
 import Project from "./Project";
+import TalkingCircles from '../public/talking-in-circles-snap.png';
+import LearningDictionaries from '../public/talking-in-circles-snap.png';
+import Reddit from '../public/talking-in-circles-snap.png';
+import SongSnake from '../public/talking-in-circles-snap.png';
 
 export default function Projects() {
 
@@ -9,15 +13,15 @@ export default function Projects() {
     siteLink: 'https://shoshbeer.github.io/talking-circles/',
     skills: ['ReactJS', 'ReduxJS', 'React-Bootstrap', 'JSX', 'React Router', 'NodeJS', 'Git'],
     description: 'Practise speaking to improve your language fluency with this 2+ player game! Set the difficulty according to your proficiency and give clues until your partner figures out the secret word.',
-    image: '/talking-in-circles-snap.png'
+    image: TalkingCircles
   },
   {
     title: 'Learning Dictionaries',
     repoLink: 'https://github.com/ShoshBeer/LearningDictionaries',
     siteLink: null,
     skills: ['Python', 'JSON', 'Various APIs', 'Git'],
-    description: 'Create a dictionary in many languages with words above a specified frequency threshold. Entries will contain definitions, the frequency, related words, and definitions of related words. Dictionaries already created for English, German, French, Spanish, Portugese, and Italian.',
-    image: '/talking-in-circles-snap.png'
+    description: 'Create dictionaries of words by frequency in different languages. Includes definitions, frequencies, and related words. Dictionaries already created for English, German, French, Spanish, Portugese, and Italian.',
+    image: LearningDictionaries
   },
   {
     title: 'Museum of Reddit Natural History',
@@ -25,15 +29,15 @@ export default function Projects() {
     siteLink: null,
     skills: ['ReactJS', 'ReduxJS', 'Collaboration', 'Reddit API', 'React-Bootstrap', 'React Router', 'NodeJS', 'Git'],
     description: 'A Chrome extension to improve the Reddit experience. No more navigating back and forth when the comments get really meta!',
-    image: '/talking-in-circles-snap.png'
+    image: Reddit
   },
   {
     title: 'Song Snake',
     repoLink: 'https://github.com/ShoshBeer/Song-Snake',
     siteLink: 'https://song-snake.netlify.app/',
     skills: ['ReactJS', 'Spotify API', 'NodeJS', 'Git'],
-    description: "Create Spotify playlists related by single words. Each song you add will trigger a new search for a single word from the title, artist, or album of the song you just added. Find music in different genres and make a game of guessing where a playlist's theme might end up!",
-    image: '/talking-in-circles-snap.png'
+    description: "Create unique Spotify playlists! Each song added triggers a new search for a single word from the title, artist, or album. Make a game of guessing where a playlist's theme will go!",
+    image: SongSnake
   },
 ];
 
